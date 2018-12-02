09ef68f55bfa82a8e6bc1679ec6b153c9f576b19
imtiaz <programmer.imtiaz@gmail.com>
2018-12-03 00:18:10 +0600

    ADD- device service checking method added

--- app/src/main/java/com/example/dipto/mygooglemap/MainActivity.java
+++ app/src/main/java/com/example/dipto/mygooglemap/MainActivity.java
@@ -1,13 +1,40 @@
 package com.example.dipto.mygooglemap;
 
+import android.app.Dialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GoogleApiAvailability;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+    public static final int ERROR_DIALOG_REQUEST = 9001 ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public boolean isServicesOK(){
+        int available = GoogleApiAvailability.getInstance().isGooglePlayServicesAvailable(MainActivity.this);
+        if(available == ConnectionResult.SUCCESS){
+            Log.d(TAG, "isServiceOK : Google Play Services is working");
+            return true ;
+        }
+        else if(GoogleApiAvailability.getInstance().isUserResolvableError(available)){
+            Log.d(TAG, "isServiceOK : Erro occured, Trying to fix it");
+            Dialog dialog = GoogleApiAvailability.getInstance().getErrorDialog(MainActivity.this,
+                    available, ERROR_DIALOG_REQUEST);
+            dialog.show();
+        }
+        else{
+            Toast.makeText(MainActivity.this, "We Can't make Map Request", Toast.LENGTH_SHORT).show();
+        }
+        return false ;
     }
 }
