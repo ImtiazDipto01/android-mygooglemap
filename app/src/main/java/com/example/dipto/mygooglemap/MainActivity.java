341c87cd62a88cc071063f9da4cbe23607e1313e
imtiaz <programmer.imtiaz@gmail.com>
2018-12-03 00:26:01 +0600

    ADD- Butterknife library added and btn Go added

--- app/src/main/java/com/example/dipto/mygooglemap/MainActivity.java
+++ app/src/main/java/com/example/dipto/mygooglemap/MainActivity.java
@@ -1,40 +1,60 @@
 package com.example.dipto.mygooglemap;
 
 import android.app.Dialog;
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.GoogleApiAvailability;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class MainActivity extends AppCompatActivity {
 
+    @BindView(R.id.btnGo)
+    Button btnGo;
+
     private static final String TAG = "MainActivity";
-    public static final int ERROR_DIALOG_REQUEST = 9001 ;
+    public static final int ERROR_DIALOG_REQUEST = 9001;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        if (isServicesOK()) {
+            init();
+        }
     }
 
-    public boolean isServicesOK(){
+    private void init() {
+        btnGo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Intent intent = new Intent(MainActivity.this, )
+            }
+        });
+    }
+
+    public boolean isServicesOK() {
         int available = GoogleApiAvailability.getInstance().isGooglePlayServicesAvailable(MainActivity.this);
-        if(available == ConnectionResult.SUCCESS){
+        if (available == ConnectionResult.SUCCESS) {
             Log.d(TAG, "isServiceOK : Google Play Services is working");
-            return true ;
-        }
-        else if(GoogleApiAvailability.getInstance().isUserResolvableError(available)){
+            return true;
+        } else if (GoogleApiAvailability.getInstance().isUserResolvableError(available)) {
             Log.d(TAG, "isServiceOK : Erro occured, Trying to fix it");
             Dialog dialog = GoogleApiAvailability.getInstance().getErrorDialog(MainActivity.this,
                     available, ERROR_DIALOG_REQUEST);
             dialog.show();
-        }
-        else{
+        } else {
             Toast.makeText(MainActivity.this, "We Can't make Map Request", Toast.LENGTH_SHORT).show();
         }
-        return false ;
+        return false;
     }
 }
