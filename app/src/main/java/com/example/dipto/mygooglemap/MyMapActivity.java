5f3815c7d5cd7519740375dff8f28a48d5043549
imtiaz <programmer.imtiaz@gmail.com>
2018-12-07 23:55:44 +0600

    ADD- MapActivity added

---
+++ app/src/main/java/com/example/dipto/mygooglemap/MyMapActivity.java
@@ -0,0 +1,13 @@
+package com.example.dipto.mygooglemap;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MyMapActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_map);
+    }
+}
