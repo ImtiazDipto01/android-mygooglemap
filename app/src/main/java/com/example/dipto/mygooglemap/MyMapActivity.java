68357386fdf55aa2664433db022edc6928f5fab9
imtiaz <programmer.imtiaz@gmail.com>
2018-12-09 23:50:18 +0600

    ADD- getDeviceLocation method created forgetting current location

--- app/src/main/java/com/example/dipto/mygooglemap/MyMapActivity.java
+++ app/src/main/java/com/example/dipto/mygooglemap/MyMapActivity.java
@@ -1,16 +1,27 @@
 package com.example.dipto.mygooglemap;
 
+import android.location.Location;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 
 public class MyMapActivity extends AppCompatActivity implements OnMapReadyCallback {
 
+    public static final float DEFAULT_ZOOM = 15f ;
     private static final String TAG = "MyMapActivity";
     private GoogleMap myMap ;
+    private FusedLocationProviderClient fusedLocationProviderClient ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,8 +36,38 @@ public class MyMapActivity extends AppCompatActivity implements OnMapReadyCallba
         mapFragment.getMapAsync(MyMapActivity.this);
     }
 
+    private void getDeviceLocation(){
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
+
+        try{
+            Task location = fusedLocationProviderClient.getLastLocation() ;
+            location.addOnCompleteListener(new OnCompleteListener() {
+                @Override
+                public void onComplete(@NonNull Task task) {
+                    if(task.isSuccessful()){
+                        Log.d(TAG, "onComplete : Location Successfull") ;
+                        Location currentLocation = (Location) task.getResult() ;
+                        moveCamera(new LatLng(currentLocation.getLatitude(), currentLocation.getLongitude()), DEFAULT_ZOOM);
+                    }
+                    else{
+                        Log.d(TAG, "onComplete : Location UnSuccessfull") ;
+                    }
+                }
+            });
+        }
+        catch (SecurityException e){
+            Log.d(TAG, "getDeviceLocation SecurityException: "+e);
+        }
+    }
+
+    private void moveCamera(LatLng latLng, float zoom) {
+        Log.d(TAG, "moveCamera : moving the camera to, lat: "+ latLng.latitude + " lng:"+ latLng.longitude);
+        myMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, zoom));
+    }
+
     @Override
     public void onMapReady(GoogleMap googleMap) {
         myMap = googleMap ;
+
     }
 }
